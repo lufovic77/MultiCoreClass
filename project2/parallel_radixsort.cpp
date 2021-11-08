409de5c0aa1bea6c92053ea8d80de34743031b0b
lufovic77 <lufovic77@gmail.com>
2021-11-08 16:42:00 +0900

    Feat: Final Version

--- project2/parallel_radixsort.cpp
+++ project2/parallel_radixsort.cpp
@@ -21,10 +21,7 @@ struct padded_char{
 
 int main(int argc, char* argv[])
 {
-    char tmpStr[30];
     int i, j, N, pos, range, ret;
-
-	struct timespec start,stop;
 
     if(argc<5){
 	    cout << "Usage: " << argv[0] << " filename number_of_strings pos range" << endl;
@@ -55,50 +52,40 @@ int main(int argc, char* argv[])
 	    return 0;
     }
 
-	clock_gettime(CLOCK_REALTIME, &start);
-
-    //auto strArr = new char[N][30];
     auto strArr = new struct padded_char[N]; 
     //string strArr[N];
 
     for(i=0; i<N; i++)
         inputfile>>strArr[i].str;
-    // |A|D|I|j|d
-    // |B| | | | 
-    // 이런식으로 저장되어 있음. 
     inputfile.close();
 
-	clock_gettime(CLOCK_REALTIME, &start);
     auto outputArr = new struct padded_char[N];
     
     //omp_set_nested(true);
 
     int len, idx; 
     for (int decimal = 29; decimal>=0; decimal--){
-        #pragma omp parallel private(i) shared(histogram, strArr, outputArr, decimal) 
+        #pragma omp parallel private(i) shared(histogram) 
         {
-            #pragma omp for private(i) schedule(static)
+            #pragma omp for schedule(static)
             for(i=0;i<59;i++)
                 histogram[i].value = 0;
   
         }
 
-        #pragma omp parallel private(i) shared(histogram, strArr, outputArr, decimal) 
+        #pragma omp parallel private(i) shared(histogram, strArr, decimal) 
         {
 
             int h_private[59] = {0};
 
-            #pragma omp for private(idx, len, i) schedule(static) 
+            #pragma omp for private(idx, len) schedule(static) 
             for (i = 0; i < N; i++){
-                //int len = strArr[i].length();
                 len = strlen(strArr[i].str);
                 if(decimal < len)
                     idx = strArr[i].str[decimal]-'A'+1;
                 else
                     idx = 0;
                 h_private[idx] ++;
-                //#pragma omp atomic
-                //histogram[idx].value+=1;
             }
             
             #pragma omp critical
@@ -108,29 +95,24 @@ int main(int argc, char* argv[])
             
         }
 
-      //      #pragma omp single
-      //      {
-                for (i = 1; i < 59; i++)
-                histogram[i].value += histogram[i-1].value;
-                for (i = N - 1; i >= 0; i--) {
-                //int len = strArr[i].length();
-                    len = strlen(strArr[i].str);
-                    if(decimal < len) 
-                        idx = strArr[i].str[decimal]-'A'+1;
-                    else
-                        idx = 0;          
-                    strncpy(outputArr[(histogram[idx].value)-1].str, strArr[i].str, 30);
-                    histogram[idx].value--;
-                }
-          //  }
+        for (i = 1; i < 59; i++)
+        histogram[i].value += histogram[i-1].value;
 
-        #pragma omp parallel private(i) shared(histogram, strArr, outputArr, decimal) 
+        for (i = N - 1; i >= 0; i--) {
+            len = strlen(strArr[i].str);
+            if(decimal < len) 
+                idx = strArr[i].str[decimal]-'A'+1;
+            else
+                idx = 0;          
+            strncpy(outputArr[(histogram[idx].value)-1].str, strArr[i].str, 30);
+            histogram[idx].value--;
+        }
+
+        #pragma omp parallel private(i) shared(strArr, outputArr, decimal) 
         {
             #pragma omp for private(i) schedule(static)
             for (i = 0; i < N; i++)
                 strncpy(strArr[i].str,outputArr[i].str,  30);
-
-            //#pragma omp barrier
         }
     }
     
@@ -140,11 +122,8 @@ int main(int argc, char* argv[])
         cout<< i << ": " << strArr[i].str<<endl;
     cout<<endl;
 
-	clock_gettime(CLOCK_REALTIME, &stop);
-
 
     delete[] strArr;
     delete[] outputArr;
-    std::cout << "Elapsed time: " << (stop.tv_sec - start.tv_sec) + ((double) (stop.tv_nsec - start.tv_nsec))/BILLION << " sec" << "\n";
     return 0;
 }
